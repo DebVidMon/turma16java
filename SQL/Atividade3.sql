4d2ffa56e6504b87b5d5a831a69436ef8c2aa104
Debora <debvm@hotmail.com>
2021-02-12 13:33:10 -0300

    Exercícios SQL

--- SQL/Atividade3.sql
+++ SQL/Atividade3.sql
@@ -11,14 +11,14 @@ nota float not null,
 primary key(id)
 );
 -- popular a tabela com 8 dados
-insert into tb_alunos(nome,idade,setor,salario) values("Julia Cintra",30,"Modelagem 3D",5.5);
-insert into tb_alunos(nome,idade,setor,salario) values("Margareth Paiva",22,"Música",8.5);
-insert into tb_alunos(nome,idade,setor,salario) values("Mônica Sousa",49,"Desenho",4.0);
-insert into tb_alunos(nome,idade,setor,salario) values("Sebastião Salgado",58,"Fotografia",10);
-insert into tb_alunos(nome,idade,setor,salario) values("Magali Carvalho",34,"Bordado",7.5);
-insert into tb_alunos(nome,idade,setor,salario) values("Ana Maria Braga",70,"Culinária",9.5);
-insert into tb_alunos(nome,idade,setor,salario) values("Sheila Mello",42,"Dança",10);
-insert into tb_alunos(nome,idade,setor,salario) values("Jonathas Santiago",46,"Mosaico",3);
+insert into tb_alunos(nome,idade,curso,nota) values("Julia Cintra",30,"Modelagem 3D",5.5);
+insert into tb_alunos(nome,idade,curso,nota) values("Margareth Paiva",22,"Música",8.5);
+insert into tb_alunos(nome,idade,curso,nota) values("Mônica Sousa",49,"Desenho",4.0);
+insert into tb_alunos(nome,idade,curso,nota) values("Sebastião Salgado",58,"Fotografia",10);
+insert into tb_alunos(nome,idade,curso,nota) values("Magali Carvalho",34,"Bordado",7.5);
+insert into tb_alunos(nome,idade,curso,nota) values("Ana Maria Braga",70,"Culinária",9.5);
+insert into tb_alunos(nome,idade,curso,nota) values("Sheila Mello",42,"Dança",10);
+insert into tb_alunos(nome,idade,curso,nota) values("Jonathas Santiago",46,"Mosaico",3);
 -- retornar nota acima de 7
 select * from tb_alunos where nota>7;
 -- retornar nota abaixo de 7
